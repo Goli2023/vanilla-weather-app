15b4d4cdd08fcf21ddc5e248aa4a2ca82903305c
Goli2023 <122437095+Goli2023@users.noreply.github.com>
2023-01-28 13:00:07 -0800

    Included API to icon

--- src/app.js
+++ src/app.js
@@ -30,13 +30,19 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let currentTime = new Date();
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  let currentTime = new Date();
   dateElement.innerHTML = formatDate(currentTime);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "0td6fef782e8b046o3c9bf5a39fbbf05";
